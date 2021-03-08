ad3ce0e2d28538a9439399c4915cee0776364858
victor <victor@victor-VirtualBox>
2021-03-08 20:42:54 +0100

    Versión inicial para el programa del examen

---
+++ src/ed/examen/tri02/ExamenEDTri02.java
@@ -0,0 +1,39 @@
+package ed.examen.tri02;
+
+/**
+ * Una clase para determinar el valor máximo de un array de enteros
+ * @author Victor Tejedor Rivas
+ */
+public class ExamenEDTri02
+{
+    /**
+     * El tamaño máximo del array permitido por el método
+     */
+    public static int TAMAÑOMAXIMO = 100;
+
+    /** 
+     * Crea una fecha a partir del día, mes y año.
+     * @param listaEnteros Un array de enteros 
+     * @return devuelve el mayor valor de un array de enteros.
+     */
+    public static int elMayorDe (int[] listaEnteros) throws Exception {
+        if (listaEnteros == null) {
+            throw new Exception ("Lista de números vacía, no se puede calcular el máximo");
+        }
+        if (listaEnteros.length > TAMAÑOMAXIMO) {
+
+        throw new Exception ("Lista de números demasido grande, no se puede calcular el máximo");
+        }
+
+        int mayor = listaEnteros[0];
+
+        for (int i = 1; i< listaEnteros.length; i++) {
+
+            if (mayor < listaEnteros[i]) {
+            mayor = listaEnteros[i];
+            }
+
+        }
+        return mayor;
+        }
+}
---
+++ test/ed/examen/tri02/ExamenEDTri02Test.java
@@ -0,0 +1,80 @@
+package ed.examen.tri02;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests para el método elMayorDe(). Comprobaremos todos los caminos posibles
+ * para determinar que el método funciona correctamente.
+ * @author Victor Tejedor Rivas
+ */
+public class ExamenEDTri02Test {
+    
+    public ExamenEDTri02Test() {
+    }
+
+    /**
+     * Test 1 para el método testElMayorDe(). Vamos a comprobar el camino 1,
+     * donde listaEnteros.length = 0.
+     * 
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void arrayVacio() {
+        System.out.println("Test para el método testElMayorDe(). Vamos a comprobar el camino 1, donde listaEnteros.length es null");
+        
+        try {
+        int[] listaEnteros = null;
+        int result = ExamenEDTri02.elMayorDe(listaEnteros);
+        }
+        catch (Exception e) {
+            System.out.println("->" + e.getMessage());
+            assertTrue(true);
+        }
+    }
+    
+    /**
+     * Test 2 para el método testElMayorDe(). Vamos a comprobar el camino 2,
+     * donde listaEnteros.length > 100.
+     * 
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void arrayDemasiadoGrande() {
+        System.out.println("Test para el método testElMayorDe(). Vamos a comprobar el camino 2, donde listaEnteros.length > 100.");
+        
+        try {
+        int[] listaEnteros = new int[101];
+        listaEnteros[45] = 7;
+        int result = ExamenEDTri02.elMayorDe(listaEnteros);
+        }
+        catch (Exception e) {
+            System.out.println("->" + e.getMessage());
+            assertTrue(true);
+        }
+    }
+    
+    /**
+     * Test 3 para el método testElMayorDe(). Vamos a comprobar el camino 3,
+     * donde (listaEnteros.length < 100) y (listaEnteros.length no es null).
+     * 
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void arrayDeTamañoValido() {
+        System.out.println("Test para el método testElMayorDe(). Vamos a comprobar el camino 3, donde listaEnteros.length esta comprendido entre 0 y 100");
+        
+        try {
+        int[] listaEnteros = new int[50];
+        listaEnteros[45] = 7;
+        listaEnteros[31] = 15;
+        listaEnteros[10] = 1;
+        int result = ExamenEDTri02.elMayorDe(listaEnteros);
+        int valorEsperado = 15;
+        assertEquals(valorEsperado, result);
+        }
+        catch (Exception e) {
+            System.out.println("->" + e.getMessage());
+        }
+    }
+}
